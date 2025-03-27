4b7f2cf3d19a837cd6143658615b8957593054c2
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2025-03-27 22:30:37 +0800

    THE GREAT STATEFUL FIX

--- icarus_ba.sh
+++ icarus_ba.sh
@@ -10,35 +10,28 @@ mexit(){
 
 get_stateful() {
 	# get_largest_cros_blockdev does not work in BadApple.
-	local ROOTDEV_LIST=$(cat /usr/sbin/write_gpt.sh | grep -w "DEFAULT_ROOTDEV" | head -n 1 | sed -E 's/^.*DEFAULT_ROOTDEV="([^"]+)".*$/\1/')
-    if [ -z "$ROOTDEV_LIST" ]; then
+	local ROOTDEV_LIST=$(cgpt find -t rootfs) # thanks stella
+	if [ -z "$ROOTDEV_LIST" ]; then
 		mexit "could not parse for rootdev devices. this should not have happened."
-    fi
-    for rootdev_device in $ROOTDEV_LIST;
-	do
-		local device_list=$(ls $rootdev_device 2> /dev/null)
-		local returncode=$?
-		[ "$returncode" -ne 0 ] && continue
-		# grep for host, nvme and mmc
-		local device_type=$(echo "$rootdev_device" | grep -oE 'mmc|nvme|host' | head -n 1)
-		case $device_type in
-		"mmc")
-			stateful=/dev/mmcblk0p1
-			break
-			;;
-		"nvme")
-			stateful=/dev/nvme0n1
-			break
-			;;
-		"host")
-			stateful=/dev/sda1
-			break
-			;;
-		*)
-			mexit "an unknown error occured. this should not have happened."
-			;;
-		esac
-	done
+	fi
+	local device_type=$(echo "$ROOTDEV_LIST" | grep -oE 'mmc|nvme|sda' | head -n 1)
+	case $device_type in
+	"mmc")
+		stateful=/dev/mmcblk0p1
+		break
+		;;
+	"nvme")
+		stateful=/dev/nvme0n1
+		break
+		;;
+	"sda")
+		stateful=/dev/sda1
+		break
+		;;
+	*)
+		mexit "an unknown error occured. this should not have happened."
+		;;
+	esac
 }
 
 does_out_exist() {
