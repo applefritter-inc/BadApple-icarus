046770ce2be6e60d192330900e2f761d263a0401
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2025-03-28 17:08:54 +0800

    ota init

---
+++ icarus_ota.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# written by appleflyer
+# ota stands for over-the-air, because this payload is intended only to be used over the air, so the user does not need to take the trouble to manually download the zip file and unzip it and stuff
+
+main(){
+}
