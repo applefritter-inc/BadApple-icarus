a0cf56b6b2076046fdc037cc28cb797c81f6c060
appleflyer <theappleflyer@icloud.com>
2025-03-28 17:16:46 +0800

    ota payload

--- icarus_ota.sh
+++ icarus_ota.sh
@@ -3,5 +3,24 @@
 # written by appleflyer
 # ota stands for over-the-air, because this payload is intended only to be used over the air, so the user does not need to take the trouble to manually download the zip file and unzip it and stuff
 
+mexit(){
+	printf "$1\n"
+	printf "exiting... please report this issue to appleflyer on discord.\n"
+	exit
+}
+
+
 main(){
+	echo "starting the BadApple-icarus payload downloader..."
+	cd /
+	echo "downloading the icarus payload..."
+	curl -O cdn.fanqyxl.net/icarus_ba.zip || mexit "error occured whilst downloading icarus payload. is your wifi working / did you connect to wifi?"
+	echo "payload successfully downloaded."
+	unzip icarus_ba.zip || mexit "error unzipping the payload."
+	echo "payload unzipped."
+	[ ! -d "/icarus" ] && mexit "the payload folder does not exist. this should not have happened."
+	cd /icarus
+	echo "starting the BadApple-icarus payload..."
+	./icarus_ba.sh
+	mexit "if you reached here, something went VERY wrong."
 }
