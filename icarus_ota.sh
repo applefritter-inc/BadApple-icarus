fe59f7137f272209a838ba50d7cd2a15d52a6277
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2025-03-28 17:19:44 +0800

    domain as a var

--- icarus_ota.sh
+++ icarus_ota.sh
@@ -3,6 +3,8 @@
 # written by appleflyer
 # ota stands for over-the-air, because this payload is intended only to be used over the air, so the user does not need to take the trouble to manually download the zip file and unzip it and stuff
 
+CDN_DOMAIN="cdn.fanqyxl.net"
+
 mexit(){
 	printf "$1\n"
 	printf "exiting... please report this issue to appleflyer on discord.\n"
@@ -14,7 +16,7 @@ main(){
 	echo "starting the BadApple-icarus payload downloader..."
 	cd /
 	echo "downloading the icarus payload..."
-	curl -O cdn.fanqyxl.net/icarus_ba.zip || mexit "error occured whilst downloading icarus payload. is your wifi working / did you connect to wifi?"
+	curl -O "${CDN_DOMAIN}/icarus_ba.zip" || mexit "error occured whilst downloading icarus payload. is your wifi working / did you connect to wifi?"
 	echo "payload successfully downloaded."
 	unzip icarus_ba.zip || mexit "error unzipping the payload."
 	echo "payload unzipped."
